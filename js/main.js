1304232cd79828960731916c543a25330fab3cb6
fabian graterol <fabiancgraterol@gmail.com>
2022-12-13 14:34:45 -0400

    refactoring getting operationValues

--- js/main.js
+++ js/main.js
@@ -62,46 +62,18 @@ resButton.addEventListener("click",() =>{
 })
 
 function calculate(){
-  let currentValue = display.innerHTML;
-  let isFirstNegative = false
-  //checking if the display has a "-" at the beggining meaning a negative number
-  const firstOperator = currentValue[0].match(/[+-]/g)
-  if (firstOperator?.length > 0){
-    if (currentValue[0] === "-"){
-      currentValue = currentValue.substr(1,currentValue.length)
-      isFirstNegative = true
-    } else {
-      //is it is other operator the make the first value a 0
-      currentValue = `0${currentValue}`
-    }
-  }
-
-
-  //getting values and operators
-  let [firstValue,secondValue] = currentValue.split(/[+-]/g);
-  let operator = currentValue.substr(firstValue.length,1)
-
-  //multipliying first value by (-1) if it was negative
-  if (isFirstNegative)
-    firstValue = parseFloat(firstValue)*(-1)
-
-  //if there is no second value and operator it means we are carrying the prev operation
-  if (!secondValue && !operator){
-    secondValue = prevOperandValue
-    operator = prevOperator
-  }
-
-  // if prev is still empty it means there is no value to make and operation,
-  // just put the first value on the display
-  if (!secondValue){
-    putValueOnDisplay(firstValue)
-  }
+  const {firstValue,secondValue,operator} = getOperationInputs()
 
   console.log({
     firstValue,secondValue,operator
   })
 
-  //now we are making the maths
+  // if prev is empty it means there is no value to make and operation,
+  // just put the first value on the display
+  if (!secondValue){
+    putValueOnDisplay(firstValue)
+  }
+
   let resp
   switch (operator) {
     case "+":
@@ -119,4 +91,38 @@ function calculate(){
   // debugger
   if (resp)
     putValueOnDisplay(resp)
+}
+
+function getOperationInputs(){
+  let currentValue = display.innerHTML;
+  let isFirstNegative = false
+  //checking if the display has a "-" at the beggining meaning a negative number
+  const firstOperator = currentValue[0].match(/[+-]/g)
+  if (firstOperator?.length > 0){
+    if (currentValue[0] === "-"){
+      currentValue = currentValue.substr(1,currentValue.length)
+      isFirstNegative = true
+    } else {
+      //is it is other operator the make the first value a 0
+      currentValue = `0${currentValue}`
+    }
+  }
+
+  //getting values and operators
+  let [firstValue,secondValue] = currentValue.split(/[+-]/g);
+  let operator = currentValue.substr(firstValue.length,1)
+
+  //multipliying first value by (-1) if it was negative
+  if (isFirstNegative)
+    firstValue = parseFloat(firstValue)*(-1)
+
+  //if there is no second value and operator it means we are carrying the prev operation
+  if (!secondValue && !operator){
+    secondValue = prevOperandValue
+    operator = prevOperator
+  }
+
+  return {
+    firstOperator,secondValue,operator
+  }
 }
